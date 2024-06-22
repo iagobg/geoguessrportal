918d2386e4edea22d80d7de55a50e75c2a75acf6
IagoBG <iago.bussoletti@gmail.com>
2024-06-22 00:26:09 -0300

    Feito

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from aiohttp import web
 from geoguessr_async  import Geoguessr
-
+import json
 
 async def index(request):
     try:
@@ -9,18 +9,19 @@ async def index(request):
         return web.json_response({"error": str(e)}, status=500)
 
 
-async def get_username(request):
+async def get_userdata(request):
     try:
-        client = Geoguessr("YOUR-NCFA-COOKIE")
-        user_info = await client.get_user_infos("YOUR-USER-ID")
-        username = user_info.nick
-        return web.json_response({"username": username})
+        client = Geoguessr("BMle7gWIuh5L+9NWsmYqqUioTWxEyvo5PvCxwO2Dapg=PKMPbEteDTmJDIYoPKlahlKhB3dFyaEzY0Whv9WDCzKW2N/BBK8CRMbp0z/aqb1mhPvjlP8r2JxU/kS3A4m3/JOwAX9YaoazVtHJWncAUXM=")
+        user_info = await client.get_user_infos("66737bea4b4ad1edf8e37921")
+        user_stats = user_info.stats
+        del user_info.stats
+        return web.json_response(vars(user_info) | vars(user_stats))
     except Exception as e:
         return web.json_response({"error": str(e)}, status=500)
     
 app = web.Application()
 app.router.add_get('/', index)
-app.router.add_get('/get-username', get_username)
+app.router.add_get('/get-userdata', get_userdata)
 
 if __name__ == '__main__':
     web.run_app(app, port=8080)
